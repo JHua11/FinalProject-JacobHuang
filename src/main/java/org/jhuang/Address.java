ea2521b3dd09186bbce584abeaa2389efd342d0f
Jacob Huang <jacob.huang.11@gmail.com>
2025-12-06 14:49:16 -0500

    Update Address class with isValidPostalCode helper method

--- src/main/java/org/jhuang/Address.java
+++ src/main/java/org/jhuang/Address.java
@@ -1,11 +1,32 @@
 package org.jhuang;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Address {
     private int streetNo;
     private String street;
     private String city;
     private Province province;
     private String postalCode;
+
+    /**
+     * checks whether a postal code is valid
+     * @param postalCode the postal code being checked
+     * @return whether the postal code is valid (true) or not (false)
+     */
+    private static boolean isValidPostalCode(String postalCode) {
+        if (postalCode.length() == 6) {
+            for (int i = 0; i < 6; i++) {
+                if (!(Character.isLetter(postalCode.charAt(i)) && i % 2 == 0
+                || Character.isDigit(postalCode.charAt(i)) && i % 2 == 1)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
 
     public enum Province {
         BC,
