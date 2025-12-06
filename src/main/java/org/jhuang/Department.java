e36f44803157c24463a3cdd447dcdfee37e88c08
Jacob Huang <jacob.huang.11@gmail.com>
2025-12-06 15:12:50 -0500

    Add isValidDepartmentName method

--- src/main/java/org/jhuang/Department.java
+++ src/main/java/org/jhuang/Department.java
@@ -4,4 +4,19 @@ public class Department {
     private String departmentId;
     private String departmentName;
     private static int nextId = 1;
+
+    /**
+     * checks if a department name is valid or not;
+     * a department name should only contain letters or space
+     * @param departmentName the name to be checked
+     * @return whether the name is valid (true) or not (false)
+     */
+    private static boolean isValidDepartmentName(String departmentName) {
+        for (char c : departmentName.toCharArray()) {
+            if (!Character.isLetter(c) && !Character.isSpaceChar(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
