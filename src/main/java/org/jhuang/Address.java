62d088936bcf68492faddef21c2ac1ce7d5d62d3
Jacob Huang <jacob.huang.11@gmail.com>
2025-12-06 13:51:11 -0500

    Add Address class and fields

---
+++ src/main/java/org/jhuang/Address.java
@@ -0,0 +1,9 @@
+package org.jhuang;
+
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+}
