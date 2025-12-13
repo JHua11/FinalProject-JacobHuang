7de5e2bcfee7b3abce5908bec3247e64a4f38bb3
Jacob Huang <jacob.huang.11@gmail.com>
2025-12-13 12:52:21 -0500

    Add Assignment class and fields

---
+++ src/main/java/org/jhuang/Assignment.java
@@ -0,0 +1,11 @@
+package org.jhuang;
+
+import java.util.List;
+
+public class Assignment {
+    private String assignmentId;
+    private String assighmentName;
+    private double weight;
+    private List<Integer> scores;
+    private static int nextId = 1;
+}
