feb50c5339e9869ce7cd9f1485761cb263a1f64d
Jacob Huang <jacob.huang.11@gmail.com>
2025-12-13 12:52:31 -0500

    Add Course class and fields

---
+++ src/main/java/org/jhuang/Course.java
@@ -0,0 +1,13 @@
+package org.jhuang;
+
+import java.util.List;
+
+public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private List<Assignment> assignments;
+    private List<Student> registeredStudents;
+    private static int nextId = 1;
+}
