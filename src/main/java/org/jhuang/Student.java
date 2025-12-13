a5b65ea35539151b4ab7e15a13e6f1857da69562
Jacob Huang <jacob.huang.11@gmail.com>
2025-12-13 16:28:23 -0500

    Add constructor, toString, toSimplifiedString

--- src/main/java/org/jhuang/Student.java
+++ src/main/java/org/jhuang/Student.java
@@ -4,6 +4,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 @EqualsAndHashCode
 @Getter
@@ -52,6 +53,40 @@ public class Student {
         registeredCourses.remove(course);
         course.getRegisteredStudents().remove(this);
         return true;
+    }
+
+    public Student(String studentName, Gender gender, Address address, Department department) {
+        this.studentId = String.format("S%06d", nextId++);
+        this.studentName = studentName;
+        this.gender = gender;
+        this.address = address;
+        this.department = department;
+        this.registeredCourses = new ArrayList<>();
+    }
+
+    public String toSimplifiedString() {
+        return "Student{" +
+                "department=" + department +
+                ", studentId='" + studentId + '\'' +
+                ", studentName='" + studentName + '\'' +
+                '}';
+    }
+
+    @Override
+    public String toString() {
+        String courseDisplay = "";
+        for (Course course : registeredCourses) {
+            courseDisplay += course.toSimplifiedString() + ", ";
+        }
+        courseDisplay = courseDisplay.substring(0,courseDisplay.length() - 2);
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", studentName='" + studentName + '\'' +
+                ", gender=" + gender +
+                ", address=" + address +
+                ", department=" + department +
+                ", registeredCourses=" + courseDisplay +
+                '}';
     }
 
     private enum Gender {
