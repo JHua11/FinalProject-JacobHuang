58c620658e63fc7c52cdd77d1f397930ebfb1bd4
Jacob Huang <jacob.huang.11@gmail.com>
2025-12-14 16:13:55 -0500

    Add lombok equals

--- src/main/java/org/jhuang/Assignment.java
+++ src/main/java/org/jhuang/Assignment.java
@@ -1,5 +1,6 @@
 package org.jhuang;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +10,7 @@ import java.util.Random;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Assignment {
     private String assignmentId;
     private String assignmentName;
