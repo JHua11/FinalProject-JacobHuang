3b60fd14d008571af97f398e13ca3b93acf51e57
Jacob Huang <jacob.huang.11@gmail.com>
2025-12-13 22:19:59 -0500

    Add registerStudent() and calcStudentsAverages()

--- src/main/java/org/jhuang/Course.java
+++ src/main/java/org/jhuang/Course.java
@@ -17,12 +17,49 @@ public class Course {
     private List<Student> registeredStudents;
     private static int nextId = 1;
 
+    /**
+     * checks if the sum of weights of all assignments of that course equals to 100%
+     * @return whether the sum is 100 (true) or not (false)
+     */
     public boolean isAssignmentWeightValid() {
         double weightSum = 0;
         for (Assignment assignment : assignments) {
             weightSum += assignment.getWeight();
         }
         return weightSum == 100;
+    }
+
+    /**
+     * adds a student to the student list of the course
+     * @param student the student to be added
+     * @return whether the operation was successful (true) or not (false)
+     */
+    public boolean registerStudent(Student student) {
+        if (registeredStudents.contains(student)) {
+            return false;
+        }
+        registeredStudents.add(student);
+        student.getRegisteredCourses().add(this);
+        for (Assignment assignment : assignments) {
+            assignment.getScores().add(null);
+        }
+        return true;
+    }
+
+    /**
+     * calculates the weighted average score of each student in a course
+     * @return the average score
+     */
+    public int[] calcStudentsAverages() {
+        int[] averages = new int[registeredStudents.size()];
+        for (int i = 0; i < registeredStudents.size(); i++) {
+            double sum = 0;
+            for (Assignment assignment : assignments) {
+                sum += assignment.getScores().get(i) * assignment.getWeight() / 100;
+            }
+            averages[i] = (int) sum;
+        }
+        return averages;
     }
 
     public String toSimplifiedString() {
